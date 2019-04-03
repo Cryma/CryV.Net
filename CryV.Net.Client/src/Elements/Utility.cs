4d2b1c2e9be27bfc46afca01a0c75aefc8ce3ce7
Fabian Vowie <crymaprivat@gmail.com>
2019-04-03 21:28:26 +0200

    Add Utility class for easier log access

---
+++ CryV.Net.Client/src/Elements/Utility.cs
@@ -0,0 +1,19 @@
+﻿using CryV.Net.Client.Native;
+
+namespace CryV.Net.Client.Elements
+{
+    public static class Utility
+    {
+
+        public static void Log(string message)
+        {
+            using (var converter = new StringConverter())
+            {
+                var messagePointer = converter.StringToPointer(message);
+
+                CryVNative.Native_Utility_Log(CryVNative.Plugin, messagePointer);
+            }
+        }
+
+    }
+}
