a4404e27f59f56d256205a3cce3d819bc4dc08a7
Fabian Vowie <crymaprivat@gmail.com>
2019-04-04 23:40:18 +0200

    Add ped behaviour natives

--- CryV.Net.Client/src/Elements/Ped.cs
+++ CryV.Net.Client/src/Elements/Ped.cs
@@ -28,6 +28,23 @@ namespace CryV.Net.Client.Elements
             SetPedDefaultComponentVariation();
 
             Streaming.SetModelAsNoLongerNeeded(model);
+
+            SetPedFleeAttributes(0, false);
+            SetBlockingOfNonTemporaryEvents(true);
+            SetPedCombatAttributes(17, true);
+            SetPedCombatAttributes(46, true);
+            SetPedSeeingRange(0.0f);
+            SetPedHearingRange(0.0f);
+            SetPedAlertness(0);
+            SetPedCanRagdoll(false);
+            SetPedCanBeTargetted(true);
+            SetPedCanEvasiveDive(false);
+            SetPedCanBeTargettedByPlayer(true);
+            SetPedGetOutUpsideDownVehicle(false);
+            SetPedAsEnemy(false);
+            SetCanAttackFriendly(true, true);
+
+            TaskSetBlockingOfNonTemporaryEvents(true);
         }
 
         public void SetPedDefaultComponentVariation()
@@ -42,6 +59,76 @@ namespace CryV.Net.Client.Elements
             Utility.Log("Ped id: " + Handle);
 
             CryVNative.Native_Ped_SetPedDefaultComponentVariation(CryVNative.Plugin, Handle);
+        }
+
+        public void SetPedFleeAttributes(int attribute, bool p2)
+        {
+            CryVNative.Native_Ped_SetPedFleeAttribute(CryVNative.Plugin, Handle, attribute, p2);
+        }
+
+        public void SetBlockingOfNonTemporaryEvents(bool toggle)
+        {
+            CryVNative.Native_Ped_SetBlockingOfNonTemporaryEvents(CryVNative.Plugin, Handle, toggle);
+        }
+
+        public void SetPedCombatAttributes(int attributesIndex, bool enabled)
+        {
+            CryVNative.Native_Ped_SetPedCombatAttributes(CryVNative.Plugin, Handle, attributesIndex, enabled);
+        }
+
+        public void SetPedSeeingRange(float range)
+        {
+            CryVNative.Native_Ped_SetPedSeeingRange(CryVNative.Plugin, Handle, range);
+        }
+
+        public void SetPedHearingRange(float range)
+        {
+            CryVNative.Native_Ped_SetPedHearingRange(CryVNative.Plugin, Handle, range);
+        }
+
+        public void SetPedAlertness(int value)
+        {
+            CryVNative.Native_Ped_SetPedAlertness(CryVNative.Plugin, Handle, value);
+        }
+
+        public void SetPedCanRagdoll(bool toggle)
+        {
+            CryVNative.Native_Ped_SetPedCanRagdoll(CryVNative.Plugin, Handle, toggle);
+        }
+
+        public void SetPedCanBeTargetted(bool toggle)
+        {
+            CryVNative.Native_Ped_SetPedCanBeTargetted(CryVNative.Plugin, Handle, toggle);
+        }
+
+        public void SetPedCanEvasiveDive(bool enable)
+        {
+            CryVNative.Native_Ped_SetPedCanEvasiveDive(CryVNative.Plugin, Handle, enable);
+        }
+
+        public void SetPedCanBeTargettedByPlayer(bool toggle)
+        {
+            CryVNative.Native_Ped_SetPedCanBeTargettedByPlayer(CryVNative.Plugin, Handle, LocalPlayer.PlayerId(), toggle);
+        }
+
+        public void SetPedGetOutUpsideDownVehicle(bool toggle)
+        {
+            CryVNative.Native_Ped_SetPedGetOutUpsideDownVehicle(CryVNative.Plugin, Handle, toggle);
+        }
+
+        public void SetPedAsEnemy(bool toggle)
+        {
+            CryVNative.Native_Ped_SetPedAsEnemy(CryVNative.Plugin, Handle, toggle);
+        }
+
+        public void SetCanAttackFriendly(bool toggle, bool p2)
+        {
+            CryVNative.Native_Ped_SetCanAttackFriendly(CryVNative.Plugin, Handle, toggle, p2);
+        }
+
+        public void TaskSetBlockingOfNonTemporaryEvents(bool toggle)
+        {
+            CryVNative.Native_Ped_TaskSetBlockingOfNonTemporaryEvents(CryVNative.Plugin, Handle, toggle);
         }
 
     }
--- CryV.Net.Client/src/Native/CryVNative.Ped.cs
+++ CryV.Net.Client/src/Native/CryVNative.Ped.cs
@@ -12,5 +12,47 @@ namespace CryV.Net.Client.Native
         [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
         public static extern int Native_Ped_CreatePed(IntPtr plugin, int pedType, ulong modelHash, float x, float y, float z, float heading, bool isNetwork, bool thisScriptCheck);
 
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedFleeAttribute(IntPtr plugin, int pedId, int attributes, bool p2);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetBlockingOfNonTemporaryEvents(IntPtr plugin, int pedId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedCombatAttributes(IntPtr plugin, int pedId, int attributesIndex, bool enabled);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedSeeingRange(IntPtr plugin, int pedId, float range);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedHearingRange(IntPtr plugin, int pedId, float range);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedAlertness(IntPtr plugin, int pedId, int value);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedCanRagdoll(IntPtr plugin, int pedId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedCanBeTargetted(IntPtr plugin, int pedId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedCanEvasiveDive(IntPtr plugin, int pedId, bool enable);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedCanBeTargettedByPlayer(IntPtr plugin, int pedId, int playerId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedGetOutUpsideDownVehicle(IntPtr plugin, int pedId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetPedAsEnemy(IntPtr plugin, int pedId, bool toggle);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_SetCanAttackFriendly(IntPtr plugin, int pedId, bool toggle, bool p2);
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern void Native_Ped_TaskSetBlockingOfNonTemporaryEvents(IntPtr plugin, int pedId, bool toggle);
+
     }
 }
