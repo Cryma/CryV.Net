ac6c9554e171a39e5f6307f62e5236ef6d1f59a5
Fabian Vowie <crymaprivat@gmail.com>
2019-04-05 18:03:30 +0200

    Add CryV.Net.Shared and CryV.Net.Server projects

---
+++ CryV.Net.Server/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryV.Net.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
