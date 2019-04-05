3aa65077656179bff246925eb6574547d6e30500
Fabian Vowie <crymaprivat@gmail.com>
2019-04-05 18:21:42 +0200

    Add simple network server

--- CryV.Net.Server/src/GameServer.cs
+++ CryV.Net.Server/src/GameServer.cs
@@ -1,16 +1,21 @@
-﻿namespace CryV.Net.Server
+﻿using CryV.Net.Server.Networking;
+
+namespace CryV.Net.Server
 {
     public class GameServer
     {
 
+        private readonly NetworkServer _networkServer;
+
         public GameServer()
         {
-
+            _networkServer = new NetworkServer(this);
+            _networkServer.Start(1337);
         }
 
         public void Tick()
         {
-
+            _networkServer?.Tick();
         }
 
     }
---
+++ CryV.Net.Server/src/Networking/NetworkServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using LiteNetLib;
+
+namespace CryV.Net.Server.Networking
+{
+    public class NetworkServer
+    {
+
+        private readonly EventBasedNetListener _listener = new EventBasedNetListener();
+        private readonly NetManager _netManager;
+
+        private readonly GameServer _gameServer;
+        private readonly int _maxPlayers;
+
+        public NetworkServer(GameServer gameServer, int maxPlayers = 32)
+        {
+            _gameServer = gameServer;
+            _maxPlayers = maxPlayers;
+
+            _listener.ConnectionRequestEvent += OnConnectionRequest;
+            _listener.PeerConnectedEvent += OnPeerConnected;
+            _listener.PeerDisconnectedEvent += OnPeerDisconnected;
+
+            _netManager = new NetManager(_listener);
+        }
+
+        private void OnConnectionRequest(ConnectionRequest request)
+        {
+            if (_netManager.PeersCount > 10)
+            {
+                request.Reject();
+
+                return;
+            }
+
+            request.AcceptIfKey("hihi");
+        }
+
+        private void OnPeerConnected(NetPeer peer)
+        {
+            Console.WriteLine($"Peer connected: {peer.Id} ({peer.EndPoint})");
+        }
+
+        private void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectinfo)
+        {
+            Console.WriteLine($"Peer disconnected: {peer.Id} ({peer.EndPoint}) - Reason: {disconnectinfo.Reason}");
+        }
+
+        public void Start(int port)
+        {
+            _netManager.Start(port);
+            Console.WriteLine("Network server started");
+        }
+
+        public void Stop()
+        {
+            _netManager.Stop();
+        }
+
+        public void Tick()
+        {
+            _netManager.PollEvents();
+        }
+
+    }
+}
