a80175c361113186ef648788a7f03b55f57c4338
Fabian Vowie <crymaprivat@gmail.com>
2019-04-06 16:40:11 +0200

    Refactor client network payload handling

--- CryV.Net.Client/src/GameClient.cs
+++ CryV.Net.Client/src/GameClient.cs
@@ -1,7 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Concurrent;
+using System.Numerics;
+using System.Threading.Tasks;
 using CryV.Net.Client.Elements;
+using CryV.Net.Client.Helpers;
 using CryV.Net.Client.Networking;
+using CryV.Net.Shared.Events;
+using CryV.Net.Shared.Events.Types;
 using CryV.Net.Shared.Payloads;
+using CryV.Net.Shared.Payloads.Helpers;
 using CryV.Net.Shared.Payloads.Partials;
 using LiteNetLib;
 using LiteNetLib.Utils;
@@ -15,9 +21,16 @@ namespace CryV.Net.Client
 
         private readonly NetworkClient _networkClient;
 
+        private readonly ConcurrentDictionary<int, Networking.Client> _clients = new ConcurrentDictionary<int, Networking.Client>();
+
         public GameClient()
         {
             _networkClient = new NetworkClient(this);
+
+            EventHandler.Subscribe<NetworkEvent<BootstrapPayload>>(OnBootstrap);
+            EventHandler.Subscribe<NetworkEvent<AddClientPayload>>(OnAddClient);
+            EventHandler.Subscribe<NetworkEvent<RemoveClientPayload>>(OnRemoveClient);
+            EventHandler.Subscribe<NetworkEvent<TransformUpdatePayload>>(OnTransformUpdate);
         }
 
         public void Connect(string address, int port)
@@ -28,11 +41,21 @@ namespace CryV.Net.Client
         public void Disconnect()
         {
             _networkClient?.Disconnect();
+
+            _clients.Clear();
+            EntityPool.Clear();
         }
 
         public void Tick()
         {
             
+        }
+
+        public void SendPayload(IPayload payload, DeliveryMethod deliveryMethod)
+        {
+            var data = PayloadHandler.SerializePayload(payload);
+
+            _networkClient.Peer.Send(data, deliveryMethod);
         }
 
         public async Task SyncTransform()
@@ -43,13 +66,68 @@ namespace CryV.Net.Client
 
                 ThreadHelper.Run(() =>
                 {
-                    var writer = new NetDataWriter();
                     var transformPayload = new TransformUpdatePayload(new ClientPayload(_networkClient.LocalId, LocalPlayer.Character.Position, LocalPlayer.Character.Rotation.Z));
-                    transformPayload.Write(writer);
 
-                    _networkClient.Peer.Send(writer, DeliveryMethod.Unreliable);
+                    SendPayload(transformPayload, DeliveryMethod.Unreliable);
                 });
             }
+        }
+
+        private void OnBootstrap(NetworkEvent<BootstrapPayload> obj)
+        {
+            _networkClient.LocalId = obj.Payload.LocalId;
+
+            ThreadHelper.Run(() =>
+            {
+                LocalPlayer.Character.Position = obj.Payload.StartPosition;
+                LocalPlayer.Character.Rotation = new Vector3(LocalPlayer.Character.Rotation.X, LocalPlayer.Character.Rotation.Y, obj.Payload.StartHeading);
+
+                foreach (var player in obj.Payload.Players)
+                {
+                    var client = new Networking.Client(player.Id, player.Position, player.Heading);
+                    _clients.TryAdd(client.Id, client);
+                }
+            });
+        }
+
+        private void OnAddClient(NetworkEvent<AddClientPayload> obj)
+        {
+            var clientData = obj.Payload.Client;
+
+            ThreadHelper.Run(() =>
+            {
+                var client = new Networking.Client(clientData.Id, clientData.Position, clientData.Heading);
+                _clients.TryAdd(client.Id, client);
+            });
+        }
+
+        private void OnRemoveClient(NetworkEvent<RemoveClientPayload> obj)
+        {
+            ThreadHelper.Run(() =>
+            {
+                if (_clients.TryRemove(obj.Payload.Id, out var client) == false)
+                {
+                    return;
+                }
+
+                client.Dispose();
+            });
+        }
+
+        private void OnTransformUpdate(NetworkEvent<TransformUpdatePayload> obj)
+        {
+            var clientData = obj.Payload.Client;
+
+            ThreadHelper.Run(() =>
+            {
+                if (_clients.TryGetValue(obj.Payload.Client.Id, out var client) == false)
+                {
+                    return;
+                }
+
+                client.Position = clientData.Position;
+                client.Rotation = new Vector3(client.Rotation.X, client.Rotation.Y, clientData.Heading);
+            });
         }
 
     }
--- CryV.Net.Client/src/Networking/NetworkClient.cs
+++ CryV.Net.Client/src/Networking/NetworkClient.cs
@@ -2,27 +2,31 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters;
 using System.Threading;
 using System.Threading.Tasks;
 using CryV.Net.Client.Elements;
 using CryV.Net.Client.Helpers;
 using CryV.Net.Shared.Enums;
+using CryV.Net.Shared.Events;
+using CryV.Net.Shared.Events.Types;
 using CryV.Net.Shared.Payloads;
+using CryV.Net.Shared.Payloads.Helpers;
 using CryV.Net.Shared.Payloads.Partials;
 using LiteNetLib;
 using LiteNetLib.Utils;
+using EventHandler = CryV.Net.Shared.Events.EventHandler;
 
 namespace CryV.Net.Client.Networking
 {
     public class NetworkClient
     {
 
-        public int LocalId { get; private set; }
+        public int LocalId { get; set; }
         public NetPeer Peer { get; private set; }
         public bool IsConnected => Peer != null;
-
-        private readonly ConcurrentDictionary<int, Client> _clients = new ConcurrentDictionary<int, Client>();
 
         private readonly EventBasedNetListener _listener = new EventBasedNetListener();
         private readonly NetManager _netManager;
@@ -44,63 +48,21 @@ namespace CryV.Net.Client.Networking
         {
             var type = (PayloadType) reader.GetByte();
 
-            if (type == PayloadType.Bootstrap)
+            var payloadObjectType = PayloadHandler.GetPayloadByType(type);
+            var payload = PayloadHandler.DeserializePayload(payloadObjectType, reader.GetRemainingBytes());
+
+            var eventType = typeof(NetworkEvent<>).MakeGenericType(payloadObjectType);
+            var eventInstance = (IEvent) FormatterServices.GetUninitializedObject(eventType);
+
+            var payloadProperty = eventType.GetProperty("Payload", BindingFlags.Public | BindingFlags.Instance);
+            if (payloadProperty == null)
             {
-                var bootstrapPacket = new BootstrapPayload();
-                bootstrapPacket.Read(reader);
-
-                ThreadHelper.Run(() =>
-                {
-                    LocalId = bootstrapPacket.LocalId;
-
-                    LocalPlayer.Character.Position = bootstrapPacket.StartPosition;
-
-                    foreach (var player in bootstrapPacket.Players)
-                    {
-                        var client = new Client(player.Id, player.Position, player.Heading);
-                        _clients.TryAdd(client.Id, client);
-                    }
-                });
+                return;
             }
-            else if (type == PayloadType.AddClient)
-            {
-                var addClientPayload = new AddClientPayload();
-                addClientPayload.Read(reader);
 
-                ThreadHelper.Run(() =>
-                {
-                    var client = new Client(addClientPayload.Client.Id, addClientPayload.Client.Position, addClientPayload.Client.Heading);
-                    _clients.TryAdd(client.Id, client);
-                });
-            }
-            else if (type == PayloadType.RemoveClient)
-            {
-                var removeClientPayload = new RemoveClientPayload();
-                removeClientPayload.Read(reader);
+            payloadProperty.SetValue(eventInstance, payload);
 
-                ThreadHelper.Run(() =>
-                {
-                    _clients.TryRemove(removeClientPayload.Id, out var client);
-
-                    client.Dispose();
-                });
-            }
-            else if (type == PayloadType.TransformUpdate)
-            {
-                var transformUpdatePayload = new TransformUpdatePayload();
-                transformUpdatePayload.Read(reader);
-
-                ThreadHelper.Run(() =>
-                {
-                    if (_clients.TryGetValue(transformUpdatePayload.Client.Id, out var client) == false)
-                    {
-                        return;
-                    }
-
-                    client.Position = transformUpdatePayload.Client.Position;
-                    client.Rotation = new Vector3(client.Rotation.X, client.Rotation.Y, transformUpdatePayload.Client.Heading);
-                });
-            }
+            EventHandler.Publish(eventType, eventInstance);
         }
 
         public void Connect(string address, int port)
@@ -133,9 +95,6 @@ namespace CryV.Net.Client.Networking
             }
 
             CancellationTokenSource.Cancel();
-
-            _clients.Clear();
-            EntityPool.Clear();
 
             _netManager.DisconnectPeer(Peer);
             Peer = null;
