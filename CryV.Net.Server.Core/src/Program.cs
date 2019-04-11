058f7269466df70d6d5227bf9888f03968c98f53
Fabian Vowie <crymaprivat@gmail.com>
2019-04-11 23:55:23 +0200

    Add client and server core project to prepare for DI

---
+++ CryV.Net.Server.Core/src/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace CryV.Net.Server.Core
+{
+    public class Program
+    {
+
+        private static ContainerManager _containerManager;
+
+        public static void Main(string[] args)
+        {
+            _containerManager = new ContainerManager();
+            _containerManager.Start();
+
+            while (true)
+            {
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
