eb96287a184472e9c5ace2d08c92b07c11ca58d8
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 22:38:57 +0200

    Add existing player sync server-side

--- CryV.Net.Server.Common/src/Interfaces/IPlayer.cs
+++ CryV.Net.Server.Common/src/Interfaces/IPlayer.cs
@@ -1,9 +1,13 @@
-﻿namespace CryV.Net.Server.Common.Interfaces
+﻿using CryV.Net.Shared.Common.Payloads;
+
+namespace CryV.Net.Server.Common.Interfaces
 {
     public interface IPlayer
     {
         
         int Id { get; }
+
+        ClientUpdatePayload GetPayload();
 
     }
 }
