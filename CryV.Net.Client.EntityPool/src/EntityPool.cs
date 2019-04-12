e424297c9f5a993adb17f1d97587f651fae3c538
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 13:50:07 +0200

    Add entity pool client module

---
+++ CryV.Net.Client.EntityPool/src/EntityPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using CryV.Net.Client.Common.Interfaces;
+using CryV.Net.Elements;
+
+namespace CryV.Net.Client.EntityPool
+{
+    public class EntityPool : IEntityPool
+    {
+
+        private static readonly ConcurrentDictionary<int, Entity> _entities = new ConcurrentDictionary<int, Entity>();
+
+        public void AddEntity(Entity entity)
+        {
+            _entities.TryAdd(entity.Handle, entity);
+        }
+
+        public void RemoveEntity(Entity entity)
+        {
+            RemoveEntity(entity.Handle);
+        }
+
+        public void RemoveEntity(int id)
+        {
+            _entities.TryRemove(id, out _);
+        }
+
+        public void Clear()
+        {
+            foreach (var entity in _entities.Values)
+            {
+                entity.Delete();
+            }
+
+            _entities.Clear();
+        }
+
+        public ICollection<Entity> GetEntities()
+        {
+            return _entities.Values.ToList();
+        }
+
+        public bool ContainsEntity(int id)
+        {
+            return _entities.ContainsKey(id);
+        }
+
+    }
+}
