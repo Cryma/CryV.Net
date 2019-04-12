92bbcda5fe254f3791d02b295eb3b2566aac0dcf
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 14:47:05 +0200

    Add payload logic

---
+++ CryV.Net.Shared.Common/src/Payloads/IPayload.cs
@@ -0,0 +1,11 @@
+﻿using CryV.Net.Shared.Common.Enums;
+
+namespace CryV.Net.Shared.Common.Payloads
+{
+    public interface IPayload
+    {
+        
+        PayloadType PayloadType { get; }
+
+    }
+}
---
+++ CryV.Net.Shared.Events/src/Types/NetworkEvent.cs
@@ -0,0 +1,21 @@
+﻿using CryV.Net.Shared.Common.Interfaces;
+using CryV.Net.Shared.Common.Payloads;
+
+namespace CryV.Net.Shared.Events.Types
+{
+    public class NetworkEvent<TPayload> : IEvent where TPayload : IPayload
+    {
+        
+        public TPayload Payload { get; set; }
+
+        public NetworkEvent()
+        {
+        }
+
+        public NetworkEvent(TPayload payload)
+        {
+            Payload = payload;
+        }
+
+    }
+}
