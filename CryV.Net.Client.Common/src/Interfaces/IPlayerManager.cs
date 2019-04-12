34837d1db70508441a5a559090130e8692b3bf0b
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 22:39:35 +0200

    Add client-side player manager and player

---
+++ CryV.Net.Client.Common/src/Interfaces/IPlayerManager.cs
@@ -0,0 +1,12 @@
+﻿using CryV.Net.Shared.Common.Payloads;
+
+namespace CryV.Net.Client.Common.Interfaces
+{
+    public interface IPlayerManager
+    {
+
+        void AddPlayer(ClientUpdatePayload player);
+        void RemovePlayer(int playerId);
+
+    }
+}
---
+++ CryV.Net.Client.Players/src/PlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using Autofac;
+using CryV.Net.Client.Common.Interfaces;
+using CryV.Net.Shared.Common.Interfaces;
+using CryV.Net.Shared.Common.Payloads;
+using CryV.Net.Shared.Events.Types;
+using LiteNetLib;
+
+namespace CryV.Net.Client.Players
+{
+    public class PlayerManager : IPlayerManager, IStartable
+    {
+
+        private readonly IEventHandler _eventHandler;
+
+        public PlayerManager(IEventHandler eventHandler)
+        {
+            _eventHandler = eventHandler;
+        }
+
+        public void Start()
+        {
+            _eventHandler.Subscribe<NetworkEvent<BootstrapPayload>>(OnBootstrap);
+        }
+
+        private void OnBootstrap(NetworkEvent<BootstrapPayload> obj)
+        {
+            foreach (var player in obj.Payload.ExistingPlayers)
+            {
+                AddPlayer(player);
+            }
+        }
+
+        public void AddPlayer(ClientUpdatePayload player)
+        {
+            
+        }
+
+        public void RemovePlayer(int playerId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
