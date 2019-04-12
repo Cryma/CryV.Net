06912e70c2239989f8d4897878f36dde3a10fb8f
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 13:29:30 +0200

    Add native helper to pass tick and keyboardtick method to modules

---
+++ CryV.Net.Client.Common/src/Helpers/NativeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CryV.Net.Client.Common.Helpers
+{
+    public static class NativeHelper
+    {
+
+        public delegate void NativeTick();
+        public delegate void KeyboardTick(ConsoleKey key, char character, bool isPressed);
+
+        public static event NativeTick OnNativeTick;
+        public static event KeyboardTick OnKeyboardTick;
+
+        public static void InvokeNativeTick()
+        {
+            OnNativeTick?.Invoke();
+        }
+
+        public static void InvokeKeyboardTick(ConsoleKey key, char character, bool isPressed)
+        {
+            OnKeyboardTick?.Invoke(key, character, isPressed);
+        }
+
+    }
+}
