a59a3eb7d46373bebcf0592ad7cccdb049519ec5
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 15:18:20 +0200

    Add server-side player connected and disconnected events

---
+++ CryV.Net.Shared.Events/src/Types/PlayerConnectedEvent.cs
@@ -0,0 +1,19 @@
+﻿using CryV.Net.Shared.Common.Interfaces;
+
+namespace CryV.Net.Shared.Events.Types
+{
+    public class PlayerConnectedEvent : IEvent
+    {
+
+        public int Id { get; set; }
+        public PlayerConnectedEvent()
+        {
+        }
+
+        public PlayerConnectedEvent(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
---
+++ CryV.Net.Shared.Events/src/Types/PlayerDisconnectedEvent.cs
@@ -0,0 +1,20 @@
+﻿using CryV.Net.Shared.Common.Interfaces;
+
+namespace CryV.Net.Shared.Events.Types
+{
+    public class PlayerDisconnectedEvent : IEvent
+    {
+        
+        public int Id { get; set; }
+
+        public PlayerDisconnectedEvent()
+        {
+        }
+
+        public PlayerDisconnectedEvent(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
