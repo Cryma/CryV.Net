b6577f83a9f8c2d3ea707318dd875375a2caced9
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 13:41:10 +0200

    Add client cleanup module

---
+++ CryV.Net.Client.Cleanup/src/CleanupModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Autofac;
+
+namespace CryV.Net.Client.Cleanup
+{
+    public class CleanupModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Cleanup>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
+
