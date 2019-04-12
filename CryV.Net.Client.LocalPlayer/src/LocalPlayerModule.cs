4cf723da2679bfb203ccae2d1413cd8f7b7a90c4
Fabian Vowie <crymaprivat@gmail.com>
2019-04-12 17:48:50 +0200

    Add local player module to client and handle bootstrapping

---
+++ CryV.Net.Client.LocalPlayer/src/LocalPlayerModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Autofac;
+
+namespace CryV.Net.Client.LocalPlayer
+{
+    public class LocalPlayerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<LocalPlayer>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
