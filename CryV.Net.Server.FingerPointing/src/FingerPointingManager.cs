6caa5a65a316089f1290227c819b49398a7438bb
Fabian Vowie <crymaprivat@gmail.com>
2019-04-13 16:33:10 +0200

    Add server-side finger pointing

---
+++ CryV.Net.Server.FingerPointing/src/FingerPointingManager.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using CryV.Net.Server.Common.Interfaces;
+using CryV.Net.Shared.Common.Interfaces;
+using CryV.Net.Shared.Common.Payloads;
+using CryV.Net.Shared.Events.Types;
+using LiteNetLib;
+
+namespace CryV.Net.Server.FingerPointing
+{
+    public class FingerPointingManager : IStartable
+    {
+
+        private readonly IEventHandler _eventHandler;
+        private readonly IPlayerManager _playerManager;
+
+        public FingerPointingManager(IEventHandler eventHandler, IPlayerManager playerManager)
+        {
+            _eventHandler = eventHandler;
+            _playerManager = playerManager;
+        }
+
+        public void Start()
+        {
+            _eventHandler.Subscribe<NetworkEvent<PointingUpdatePayload>>(OnPointingUpdate);
+            _eventHandler.Subscribe<NetworkEvent<StopPointingPayload>>(OnStopPointing);
+        }
+
+        private void OnPointingUpdate(NetworkEvent<PointingUpdatePayload> obj)
+        {
+            var player = _playerManager.GetPlayer(obj.Payload.Id);
+
+            if (player == null)
+            {
+                return;
+            }
+
+            foreach (var otherPlayer in _playerManager.GetPlayers())
+            {
+                if (otherPlayer == player)
+                {
+                    continue;
+                }
+
+                otherPlayer.Send(obj.Payload, DeliveryMethod.Unreliable);
+            }
+        }
+
+        private void OnStopPointing(NetworkEvent<StopPointingPayload> obj)
+        {
+            var player = _playerManager.GetPlayer(obj.Payload.Id);
+
+            if (player == null)
+            {
+                return;
+            }
+
+            foreach (var otherPlayer in _playerManager.GetPlayers())
+            {
+                if (otherPlayer == player)
+                {
+                    continue;
+                }
+
+                otherPlayer.Send(obj.Payload, DeliveryMethod.Unreliable);
+            }
+        }
+
+    }
+}
