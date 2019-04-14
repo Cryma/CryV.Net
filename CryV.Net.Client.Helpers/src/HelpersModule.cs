1e355cbf5db23e65e3551d1cc9a585da71702600
Fabian Vowie <crymaprivat@gmail.com>
2019-04-14 21:59:22 +0200

    Add client-side helper project

---
+++ CryV.Net.Client.Helpers/src/HelpersModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Autofac;
+
+namespace CryV.Net.Client.Helpers
+{
+    public class HelpersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PauseMenuHelper>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+    }
+}
