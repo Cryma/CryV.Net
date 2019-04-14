090de33a5918407379f55f1c572e711513abbc2e
Fabian Vowie <crymaprivat@gmail.com>
2019-04-14 21:59:29 +0200

    Add pause menu helper

---
+++ CryV.Net.Client.Helpers/src/PauseMenuHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Numerics;
+using Autofac;
+using CryV.Net.Client.Common.Helpers;
+using CryV.Net.Elements;
+using CryV.Net.Enums;
+
+namespace CryV.Net.Client.Helpers
+{
+    public class PauseMenuHelper : IStartable
+    {
+
+        public void Start()
+        {
+            NativeHelper.OnNativeTick += Tick;
+        }
+
+        private void Tick(float deltatime)
+        {
+            UserInterface.DrawText("CryV", new Vector2(0.975f, 0.01f), 0.42f, Color.FromArgb(255, 200, 200, 200), TextFont.ChaletLondon, TextAlignment.Center, 1.0f);
+
+            Gameplay.DisableControlAction(0, 199, true);
+            Gameplay.DisableControlAction(0, 200, true);
+
+            if (Gameplay.IsDisabledControlJustPressed(0, 200))
+            {
+                UserInterface.ActivateFrontendMenu(3123948979, false, -1);
+            }
+        }
+
+    }
+}
