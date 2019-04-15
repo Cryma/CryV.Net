29c1737cc177594361540454f84c120fc725f677
Fabian Vowie <crymaprivat@gmail.com>
2019-04-16 00:41:12 +0200

    Add prop creation native

---
+++ CryV.Net/src/Elements/Prop.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using CryV.Net.Native;
+
+namespace CryV.Net.Elements
+{
+    public class Prop : Entity
+    {
+        public Prop(int handle) : base(handle)
+        {
+        }
+
+        public Prop(uint model, Vector3 position) : base(0)
+        {
+
+        }
+
+        private void CreateProp(uint model, Vector3 position)
+        {
+            Streaming.LoadModel(model);
+
+            Handle = CryVNative.Native_Prop_CreateObject(CryVNative.Plugin, model, position.X, position.Y, position.Z, true, true, false);
+
+            Streaming.UnloadModel(model);
+        }
+
+    }
+}
---
+++ CryV.Net/src/Native/CryVNative.Prop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CryV.Net.Native
+{
+    internal static partial class CryVNative
+    {
+
+        [DllImport(_dllLocation, CallingConvention = CallingConvention.StdCall)]
+        public static extern int Native_Prop_CreateObject(IntPtr plugin, ulong hash, float x, float y, float z, bool isNetwork, bool thisScriptCheck, bool dynamic);
+
+    }
+}
