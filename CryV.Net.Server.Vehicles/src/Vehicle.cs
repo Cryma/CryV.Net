673655e36d8231673ff37cdbfb08a8ad72970ec8
Fabian Vowie <crymaprivat@gmail.com>
2019-04-17 22:59:43 +0200

    Add server-side vehicle add functionality

--- CryV.Net.Server.Vehicles/src/Vehicle.cs
+++ CryV.Net.Server.Vehicles/src/Vehicle.cs
@@ -1,9 +1,38 @@
-﻿using CryV.Net.Server.Common.Interfaces;
+﻿using System.Numerics;
+using CryV.Net.Server.Common.Interfaces;
+using CryV.Net.Shared.Common.Payloads;
 
 namespace CryV.Net.Server.Vehicles
 {
     public class Vehicle : IVehicle
     {
         
+        public int Id { get; }
+
+        public Vector3 Position { get; set; }
+
+        public Vector3 Velocity { get; set; }
+
+        public Vector3 Rotation { get; set; }
+
+        public Vehicle(int id, Vector3 position, Vector3 rotation)
+        {
+            Id = id;
+            Position = position;
+            Rotation = rotation;
+        }
+
+        public VehicleUpdatePayload GetPayload()
+        {
+            return new VehicleUpdatePayload(Id, Position, Velocity, Rotation);
+        }
+
+        public void ReadPayload(VehicleUpdatePayload payload)
+        {
+            Position = payload.Position;
+            Velocity = payload.Velocity;
+            Rotation = payload.Rotation;
+        }
+
     }
 }
--- CryV.Net.Server.Vehicles/src/VehicleManager.cs
+++ CryV.Net.Server.Vehicles/src/VehicleManager.cs
@@ -1,9 +1,50 @@
-﻿using CryV.Net.Server.Common.Interfaces;
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using Autofac;
+using CryV.Net.Server.Common.Interfaces;
+using CryV.Net.Shared.Common.Interfaces;
 
 namespace CryV.Net.Server.Vehicles
 {
-    public class VehicleManager : IVehicleManager
+    public class VehicleManager : IVehicleManager, IStartable
     {
-        
+
+        private readonly IEventHandler _eventHandler;
+        private readonly INetworkManager _networkManager;
+
+        private readonly ConcurrentDictionary<int, IVehicle> _vehicles = new ConcurrentDictionary<int, IVehicle>();
+
+        public VehicleManager(IEventHandler eventHandler, INetworkManager networkManager)
+        {
+            _eventHandler = eventHandler;
+            _networkManager = networkManager;
+        }
+
+        public void Start()
+        {
+            // TODO: Remove debug vehicle
+            AddVehicle(new Vector3(165.1652f, -1064.867f, 29.19238f), Vector3.Zero);
+        }
+
+        public IVehicle AddVehicle(Vector3 position, Vector3 rotation)
+        {
+            var id = GetFreeId();
+
+            var vehicle = new Vehicle(id, position, rotation);
+
+            _vehicles.TryAdd(id, vehicle);
+
+            return vehicle;
+        }
+
+        private int GetFreeId()
+        {
+            return Enumerable.Range(0, int.MaxValue)
+                .Except(_vehicles.Keys.ToArray())
+                .First();
+        }
+
     }
 }
