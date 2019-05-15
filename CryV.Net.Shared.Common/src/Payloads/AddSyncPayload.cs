ac5859f3aa7ab6446da03cc2627f91a568293c6f
Fabian Vowie <crymaprivat@gmail.com>
2019-05-15 16:01:58 +0200

    Add sync management payloads

---
+++ CryV.Net.Shared.Common/src/Payloads/AddSyncPayload.cs
@@ -0,0 +1,24 @@
+﻿using CryV.Net.Shared.Common.Enums;
+using ProtoBuf;
+
+namespace CryV.Net.Shared.Common.Payloads
+{
+    [ProtoContract]
+    public class AddSyncPayload : IPayload
+    {
+        public PayloadType PayloadType { get; } = PayloadType.AddSync;
+
+        [ProtoMember(1)]
+        public int EntityId { get; set; }
+
+        public AddSyncPayload()
+        {
+        }
+
+        public AddSyncPayload(int entityId)
+        {
+            EntityId = entityId;
+        }
+
+    }
+}
---
+++ CryV.Net.Shared.Common/src/Payloads/RemoveSyncPayload.cs
@@ -0,0 +1,24 @@
+﻿using CryV.Net.Shared.Common.Enums;
+using ProtoBuf;
+
+namespace CryV.Net.Shared.Common.Payloads
+{
+    [ProtoContract]
+    public class RemoveSyncPayload : IPayload
+    {
+        public PayloadType PayloadType { get; } = PayloadType.RemoveSync;
+
+        [ProtoMember(1)]
+        public int EntityId { get; set; }
+
+        public RemoveSyncPayload()
+        {
+        }
+
+        public RemoveSyncPayload(int entityId)
+        {
+            EntityId = entityId;
+        }
+
+    }
+}
