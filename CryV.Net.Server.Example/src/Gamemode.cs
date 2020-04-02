7eb0532dd4adb8cfbe7f9511d634d85e6ad90acf
Fabian Vowie <crymaprivat@gmail.com>
2020-04-02 22:12:03 +0200

    Reposition vehicles in example server gamemode

--- CryV.Net.Server.Example/src/Gamemode.cs
+++ CryV.Net.Server.Example/src/Gamemode.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Numerics;
 using CryV.Net.Server.Api.Scripting;
 using CryV.Net.Server.Common.Interfaces;
@@ -19,12 +19,12 @@ namespace CryV.Net.Server.Example
             MP.Events.OnPlayerConnected += OnPlayerConnected;
             MP.Events.OnPlayerDisconnected += OnPlayerDisconnected;
 
-            MP.VehiclePool.CreateVehicle(new Vector3(165.1652f, -1077.867f, 28.433891f), Vector3.Zero, 1274868363, "ehre");
-            MP.VehiclePool.CreateVehicle(new Vector3(161.1652f, -1077.867f, 28.433891f), Vector3.Zero, 2364918497, "1337");
-            MP.VehiclePool.CreateVehicle(new Vector3(157.1652f, -1077.867f, 28.433891f), Vector3.Zero, 4180675781);
-            MP.VehiclePool.CreateVehicle(new Vector3(154.1652f, -1077.867f, 28.433891f), Vector3.Zero, 0xCBB2BE0E);
+            MP.VehiclePool.CreateVehicle(new Vector3(104.39481f, -1078.5488f, 28.43744f), new Vector3(0.0f, 0.0f, -19.036509f), 1274868363, "ehre");
+            MP.VehiclePool.CreateVehicle(new Vector3(107.84033f, -1079.7783f, 28.64444f), new Vector3(0.0f, 0.0f, -19.036509f), 2364918497, "1337");
+            MP.VehiclePool.CreateVehicle(new Vector3(111.060104f, -1049.2864f, 28.566235f), new Vector3(0.0f, 0.0f, -114.83127f), 4180675781);
+            MP.VehiclePool.CreateVehicle(new Vector3(107.0332f, -1063.8341f, 29.297525f), new Vector3(0.0f, 0.0f, -113.37058f), 0x21EEE87D);
             //MP.VehiclePool.CreateVehicle(new Vector3(151.1652f, -1077.867f, 28.433891f), Vector3.Zero, 1912215274);
-            MP.VehiclePool.CreateVehicle(new Vector3(148.1652f, -1077.867f, 28.433891f), Vector3.Zero, 0x21EEE87D);
+            MP.VehiclePool.CreateVehicle(new Vector3(157.44566f, -1081.4004f, 31.05618f), new Vector3(0.0f, 0.0f, 90.253784f), 0xCBB2BE0E);
 
             _logger.LogInformation("Started example gamemode!");
         }
